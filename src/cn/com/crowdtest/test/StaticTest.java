ce787b21890ce0bc5d5bfd94b0855a50d2c5d236
Shawn <shawnjust93@gmail.com>
2014-04-30 15:58:36 +0800

    Commit All

---
+++ src/cn/com/crowdtest/test/StaticTest.java
@@ -0,0 +1,32 @@
+package cn.com.crowdtest.test;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import cn.com.crowdsourcedtesting.DAO.HibernateSessionFactory;
+import cn.com.crowdsourcedtesting.bean.Tester;
+
+public class StaticTest {
+	public static void main(String[] argv) {
+
+		Session session = HibernateSessionFactory.getSession();
+		Transaction tx = null;
+		try {
+			tx = session.beginTransaction();
+			Tester tester = new Tester();
+			tester.setTesterEmail("Shawn103@qq.com");
+			tester.setTesterPassword("123456");
+			tester.setTesterName("Shawn103");
+			session.save(tester);
+			tx.commit();
+		} catch (RuntimeException e) {
+			e.printStackTrace();
+			if (tx != null) {
+				tx.rollback();
+			}
+		} finally {
+			session.close();
+		}
+		
+	}
+}
